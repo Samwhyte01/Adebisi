e489369395d4ce1c6a2257754ea608696024a43d
Samwhyte01 <115500021+Samwhyte01@users.noreply.github.com>
2022-10-26 15:56:36 +0100

    Add files via upload

---
+++ new.js
@@ -0,0 +1,13 @@
+const notification= document. querySelector("#J");
+const notify= document.querySelector("#JK");
+const notifi= document.querySelector("#JC");
+const read= document.querySelector("#read");
+
+const display= document.querySelector(".read");
+read.addEventListener("click",()=>{
+    notification.style.backgroundcolor="white";
+    notify.style.backgroundcolor="white";
+    notify.style.backgroundcolor="white";
+    display.textContent=0;
+    favicon.style.backgroundcolor="green"
+});
